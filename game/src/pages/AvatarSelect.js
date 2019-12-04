03376069986c2a210391dff93e2d03b5b0f3264a
Parthey Khanderia <parthey_khanderia@student.uml.edu>
2019-12-04 15:27:13 -0500

    Added lessons and avatar image

--- game/src/pages/AvatarSelect.js
+++ game/src/pages/AvatarSelect.js
@@ -1,17 +1,50 @@
 import React, { Component } from "react";
 import {Button} from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Image from 'react-bootstrap/Image';
+import hero_orb from './hero_orb.jpg';
 
 export default function AvatarSelect() {
   return (
     <div className="avatar-selection">
       <h1>Select your Avatar</h1>
-      <Link to="/select-lesson">
+   
+       
+      <br></br>
+      <br></br>
+     <div>
+        <Link to="/select-lesson">
         Avatar 1
       </Link>
+      <img src={hero_orb} alt="hero orb" height="100"/>
+     </div>
+     <br></br>
+     <br></br>
+     <div>
       <Link to="/select-lesson">
         Avatar 2
       </Link>
+      <img src={hero_orb} alt="hero orb" height="100"/>
+      </div>
+      <br></br>
+     <br></br>
+      <div>
+      <Link to="/select-lesson">
+        Avatar 3
+      </Link>
+      <img src={hero_orb} alt="hero orb" height="100"/>
+      </div>
+      <br></br>
+     <br></br>
+      <div>
+      <Link to="/select-lesson">
+        Avatar 4
+      </Link>
+      <img src={hero_orb} alt="hero orb" height="100"/>
+      </div>
+      <br></br>
+     <br></br>
+      
     </div>
   );
 }
--- game/src/pages/LessonSelect.js
+++ game/src/pages/LessonSelect.js
@@ -12,6 +12,24 @@ export default function LessonSelect() {
       <Button to="/lesson2" renderAs={Link}>
         Lesson 2
       </Button>
+      <Button to="/lesson2" renderAs={Link}>
+        Lesson 3
+      </Button>
+      <Button to="/lesson2" renderAs={Link}>
+        Lesson 4
+      </Button>
+      <Button to="/lesson2" renderAs={Link}>
+        Lesson 5
+      </Button>
+      <Button to="/lesson2" renderAs={Link}>
+        Lesson 6
+      </Button>
+      <Button to="/lesson2" renderAs={Link}>
+        Lesson 7
+      </Button>
+      <Button to="/lesson2" renderAs={Link}>
+        Lesson 8
+      </Button>
     </div>
   );
 }
