978d94be7cab3819acdfde77dcce29f68f25f597
Parthey Khanderia <parthey_khanderia@student.uml.edu>
2019-12-04 23:29:50 -0500

    add stuff

--- game/src/pages/AvatarSelect.js
+++ game/src/pages/AvatarSelect.js
@@ -3,13 +3,13 @@ import {Button} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Image from 'react-bootstrap/Image';
 import hero_orb from './hero_orb.jpg';
+import Beastmode from  './Beastmode.jpg';
 
 export default function AvatarSelect() {
   return (
     <div className="avatar-selection">
-      <h1>Select your Avatar</h1>
-   
-       
+      <h1>Choose your Avatar</h1>
+      <h2 >Here you can choose one of these 4 avatars</h2>
       <br></br>
       <br></br>
      <div>
@@ -24,7 +24,7 @@ export default function AvatarSelect() {
       <Link to="/select-lesson">
         Avatar 2
       </Link>
-      <img src={hero_orb} alt="hero orb" height="100"/>
+      <img src ={Beastmode} alt="hero orb" height="100"/>
       </div>
       <br></br>
      <br></br>
@@ -40,7 +40,7 @@ export default function AvatarSelect() {
       <Link to="/select-lesson">
         Avatar 4
       </Link>
-      <img src={hero_orb} alt="hero orb" height="100"/>
+      <img src={Beastmode} alt="hero orb" height="100"/>
       </div>
       <br></br>
      <br></br>
--- game/src/pages/LessonSelect.js
+++ game/src/pages/LessonSelect.js
@@ -5,30 +5,30 @@ import {Button} from "react-bootstrap";
 export default function LessonSelect() {
   return (
     <div className="lesson-selection">
-      <h1>Select Lesson</h1>
+      <h1>Here are 8 Lessons to pick from. Pick a lesson to see the PSG and answer questions</h1>
       <Button to="/lesson1" renderAs={Link}>
-        Lesson 1
+        (1)Security
       </Button>
       <Button to="/lesson2" renderAs={Link}>
-        Lesson 2
+        (2)Digital Downtime
       </Button>
       <Button to="/lesson2" renderAs={Link}>
-        Lesson 3
+        (3)Etiquette
       </Button>
       <Button to="/lesson2" renderAs={Link}>
-        Lesson 4
+        (4)Health
       </Button>
       <Button to="/lesson2" renderAs={Link}>
-        Lesson 5
+        (5)Integrity
       </Button>
       <Button to="/lesson2" renderAs={Link}>
-        Lesson 6
+        (6)Privacy
       </Button>
       <Button to="/lesson2" renderAs={Link}>
-        Lesson 7
+        (7)Responsibility
       </Button>
       <Button to="/lesson2" renderAs={Link}>
-        Lesson 8
+        (8)Literacy
       </Button>
     </div>
   );
