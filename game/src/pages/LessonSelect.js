cc143641a745ea98829b9f762651c46ca9041b8f
Parthey Khanderia <parthey_khanderia@student.uml.edu>
2019-12-10 01:43:55 -0500

    disable and align the non working buttons in lesson

--- game/src/pages/LessonSelect.js
+++ game/src/pages/LessonSelect.js
@@ -22,100 +22,97 @@ import Literacy from"./literacy.png"
 
 export default function LessonSelect() { return (
   <div className="lesson-selection">
-      <AppBar position="static">
-          <Toolbar variant="dense">
-              <Typography variant="h6" color="inherit">
+  <AppBar position="static">
+      <Toolbar variant="dense">
+          <Typography variant="h6" color="inherit">
   
-                  DigiSafe - Select Lesson
-              </Typography>
-          </Toolbar>
-      </AppBar>
-      <br />
-      <Grid container direction="row" justify="center" spacing={1}>
-          <Grid item>
-              <Paper style={{ textAlign: "center", justifyContent: "center", alignItems: "center" }}>
-                  <Button variant="contained" color="primary" href="/Security1" align="center">
-                      Security
-                  </Button>
-                  <br />
-                  <img src={security_bckg} alt="Security background" height="225" width="350" />
-              </Paper>
-          </Grid>
-  
-          <Grid item>
-  
-              <Paper style={{ textAlign: "center", justifyContent: "center", alignItems: "center" }}>
-                  <Button variant="contained" color="primary" align="center">
-                      Etiquette
-                  </Button>
-  
-                  <br />
-                  <img src={Etiquette} alt="Security background" height="225" width="350" />
-              </Paper>
-          </Grid>
-  
-          <Grid item>
-              <Paper style={{ textAlign: "center", justifyContent: "center", alignItems: "center" }}>
-                  <Button variant="contained" color="primary" align="center">
-                      Digital Downtown
-                  </Button>
-                  <br />
-                  <img src={Downtime} alt="Security background" height="225" width="350" />
-              </Paper>
-          </Grid>
-  
-          <Grid item>
-              <Paper style={{ textAlign: "center", justifyContent: "center", alignItems: "center" }}>
-                  <Button variant="contained" color="primary" align="center">
-                      Health
-                  </Button>
-                  <br />
-                  <img src={Health} alt="Security background" height="225" width="350" />
-              </Paper>
-          </Grid>
-          <Grid item>
-              <Paper style={{ textAlign: "center", justifyContent: "center", alignItems: "center" }}>
-                  <Button variant="contained" color="primary" align="center">
-                      Integrity
-                  </Button>
-                  <br />
-                  <img src={Integrity} alt="Security background" height="225" width="350" />
-              </Paper>
-          </Grid>
-  
-          <Grid item>
-              <Paper style={{ textAlign: "center", justifyContent: "center", alignItems: "center" }}>
-                  <Button variant="contained" color="primary" align="center">
-                      Privacy
-                  </Button>
-                  <br />
-                  <img src={Privacy} alt="Security background" height="225" width="350" />
-              </Paper>
-          </Grid>
-          <Grid item>
-              <Paper style={{ textAlign: "center", justifyContent: "center", alignItems: "center" }}>
-                  <Button variant="contained" color="primary" align="center">
-                      Responsibility
-                  </Button>
-                  <br />
-                  <img src={Responsibility} alt="Security background" height="225" width="350" />
-              </Paper>
-          </Grid>
-          <Grid item>
-              <Paper style={{ textAlign: "center", justifyContent: "center", alignItems: "center" }}>
-                  <Button variant="contained" color="primary" align="center">
-                      Literacy
-                  </Button>
-                  <br />
-                  <img src={Literacy} alt="Security background" height="225" width="350" />
-              </Paper>
-          </Grid>
-  
+              DigiSafe - Select Lesson
+          </Typography>
+      </Toolbar>
+  </AppBar>
+  <br />
+  <Grid container direction="row" justify="center" spacing={1}>
+      <Grid item>
+          <Paper style={{ textAlign: "center", justifyContent: "center", alignItems: "center" }}>
+              <Button variant="contained" color="primary" href="/Security1" align="center">
+                  Security
+              </Button>
+              <br />
+              <img src={security_bckg} alt="Security background" height="225" width="350" />
+          </Paper>
       </Grid>
   
-      {/*
+      <Grid item>
   
-      <Link to="/lesson2">Responsibility</Link>
-      <Link to="/lesson2">Literacy</Link> */}
+          <Paper style={{ textAlign: "center", justifyContent: "center", alignItems: "center" }}>
+  
+              <Button variant="contained" color="primary" align="center" disabled="true">
+                  Etiquette
+              </Button>
+  
+              <br />
+              <img src={Etiquette} alt="Security background" height="225" width="350" />
+          </Paper>
+      </Grid>
+  
+      <Grid item>
+          <Paper style={{ textAlign: "center", justifyContent: "center", alignItems: "center" }}>
+              <Button variant="contained" color="primary" align="center" disabled="true">
+                  Digital Downtown
+              </Button>
+              <br />
+              <img src={Downtime} alt="Security background" height="225" width="350" />
+          </Paper>
+      </Grid>
+  
+      <Grid item>
+          <Paper style={{ textAlign: "center", justifyContent: "center", alignItems: "center" }}>
+              <Button variant="contained" color="primary" align="center" disabled="true">
+                  Health
+              </Button>
+              <br />
+              <img src={Health} alt="Security background" height="225" width="350" />
+          </Paper>
+      </Grid>
+      <Grid item>
+          <Paper style={{ textAlign: "center", justifyContent: "center", alignItems: "center" }}>
+              <Button variant="contained" color="primary" align="center" disabled="true">
+                  Integrity
+              </Button>
+              <br />
+              <img src={Integrity} alt="Security background" height="225" width="350" />
+          </Paper>
+      </Grid>
+  
+      <Grid item>
+          <Paper style={{ textAlign: "center", justifyContent: "center", alignItems: "center" }}>
+              <Button variant="contained" color="primary" align="center" disabled="true">
+                  Privacy
+              </Button>
+              <br />
+              <img src={Privacy} alt="Security background" height="225" width="350" />
+          </Paper>
+      </Grid>
+      <Grid item>
+          <Paper style={{ textAlign: "center", justifyContent: "center", alignItems: "center" }}>
+              <Button variant="contained" color="primary" align="center" disabled="true">
+                  Responsibility
+              </Button>
+              <br />
+              <img src={Responsibility} alt="Security background" height="225" width="350" />
+          </Paper>
+      </Grid>
+      <Grid item>
+          <Paper style={{ textAlign: "center", justifyContent: "center", alignItems: "center" }}>
+              <Button variant="contained" color="primary" align="center" disabled="true">
+                  Literacy
+              </Button>
+              <br />
+              <img src={Literacy} alt="Security background" height="225" width="350" />
+          </Paper>
+      </Grid>
+  
+  </Grid>
+  
   </div>
   ); }
